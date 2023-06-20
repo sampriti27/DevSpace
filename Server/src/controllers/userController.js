1b5b3ddb52be277fbf44021f34c4e32a448aaa91
Sampriti Mukherjee <sampriti275@gmail.com>
2023-06-20 11:59:42 +0530

    register feature added

--- Server/src/controllers/userController.js
+++ Server/src/controllers/userController.js
@@ -1,11 +1,75 @@
 const asyncHandler = require("express-async-handler");
+const User = require("../models/user/userModel");
+const bcrypt = require("bcrypt");
 
 //Desc Register users
 //@route POST /api/user/register
 //@access public
 
 const register = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Register route!" });
+  const { name, email, username, password, cPassword } = req.body;
+
+  // validate
+  if (!name || !email || !username || !password || !cPassword) {
+    res.status(400);
+
+    throw new Error("Please fill all the fields!");
+  }
+
+  // check for existing users
+  const userAvailable = await User.findOne({ email });
+
+  if (userAvailable) {
+    res.status(400);
+
+    throw new Error("User is already registered with this emailId!");
+  }
+
+  // check if username is already taken
+  const invalidUsername = await User.findOne({ username });
+
+  if (invalidUsername) {
+    res.status(400);
+
+    throw new Error(
+      "username already taken! Please use another username for this account!"
+    );
+  }
+
+  // match password and confirm password
+
+  if (password === cPassword) {
+    // password hashing
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const hashedCpassword = await bcrypt.hash(cPassword, 10);
+
+    // create a new user
+    const newUser = await User.create({
+      name,
+      email,
+      username,
+      password: hashedPassword,
+      cPassword: hashedCpassword,
+    });
+
+    newUser.password = undefined;
+    newUser.cPassword = undefined;
+
+    console.log(`New user created ${newUser}`);
+    if (newUser) {
+      res.status(201).json(newUser);
+    } else {
+      res.status(400);
+
+      throw new Error("User data is invalid!");
+    }
+  } else {
+    res.status(400);
+
+    throw new Error("passwords do not match! please fill again!");
+  }
+
+  res.status(200).json({ message: "new user registered successfully!" });
 });
 
 //Desc Login users
--- Server/src/models/user/userModel.js
+++ Server/src/models/user/userModel.js
@@ -2,88 +2,81 @@ const mongoose = require("mongoose");
 
 // creating the mongoDb user model
 
-const userSchema = mongoose.Schema({
-  fullName: {
-    type: String,
-    required: [true, "Please add your full name."],
-  },
-  email: {
-    type: String,
-    required: [true, "Please add your email address."],
-    unique: [true, "Email address already taken"],
-    validate: {
-      validator: function (v) {
-        return /\S+@\S+\.\S+/.test(v); // validate that email is in a valid format
-      },
-      message: "Email must be in a valid format",
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please add your full name."],
     },
-  },
-  username: {
-    type: String,
-    required: [true, "Please select a valid userId"],
-    unique: [true, "userId already taken!"],
-  },
-  password: {
-    type: String,
-    required: true,
-    validate: {
-      validator: function (v) {
-        // The password must be at least 8 characters long
-        // and contain at least one uppercase letter, one lowercase letter,
-        // one digit, and one special character.
-        return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(
-          v
-        );
+    email: {
+      type: String,
+      required: [true, "Please add your email address."],
+      unique: [true, "Email address already taken"],
+      validate: {
+        validator: function (v) {
+          return /\S+@\S+\.\S+/.test(v); // validate that email is in a valid format
+        },
+        message: "Email must be in a valid format",
       },
-      message:
-        "Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one digit, and one special character.",
     },
-  },
-  cPassword: {
-    type: String,
-    required: true,
-    validate: {
-      validator: function (v) {
-        // The password must be at least 8 characters long
-        // and contain at least one uppercase letter, one lowercase letter,
-        // one digit, and one special character.
-        return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(
-          v
-        );
+    username: {
+      type: String,
+      required: [true, "Please select a valid username"],
+      unique: [true, "username already taken!"],
+    },
+    password: {
+      type: String,
+      required: true,
+      validate: {
+        validator: function (v) {
+          // The password must be at least 8 characters long
+          // and contain at least one uppercase letter, one lowercase letter,
+          // one digit, and one special character.
+          return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(
+            v
+          );
+        },
+        message:
+          "Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one digit, and one special character.",
       },
-      message:
-        "Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one digit, and one special character.",
     },
-  },
-  phone: {
-    type: Number,
+    cPassword: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: Number,
 
-    validate: {
-      validator: function (v) {
-        return /\d{10}/.test(v); // validate that number is a 10-digit number
+      validate: {
+        validator: function (v) {
+          return /\d{10}/.test(v); // validate that number is a 10-digit number
+        },
+        message: "Phone number must be a 10-digit number",
       },
-      message: "Phone number must be a 10-digit number",
+    },
+    gender: {
+      type: String,
+    },
+    dob: {
+      type: Date,
+    },
+    photo: {
+      type: String,
+    },
+    bio: {
+      type: String,
+      validate: {
+        validator: function (v) {
+          const words = v.trim().split(/\s+/);
+          return words.length <= 200;
+        },
+        message: "Bio should not exceed 200 words.",
+      },
     },
   },
-  gender: {
-    type: String,
-  },
-  dob: {
-    type: Date,
-  },
-  photo: {
-    type: String,
-  },
-  bio: {
-    type: String,
-    validate: {
-      validator: function (v) {
-        const words = v.trim().split(/\s+/);
-        return words.length <= 200;
-      },
-      message: "Bio should not exceed 200 words.",
-    },
-  },
-});
+  {
+    timestamp: true,
+  }
+);
 
 module.exports = mongoose.model("User", userSchema);
